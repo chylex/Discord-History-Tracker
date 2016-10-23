2ee1f6550b37185798209e8863924c7323bb81c0
chylex <info@chylex.com>
2016-10-23 19:25:26 +0200

    Add savefile.js with the database format specifications

---
+++ src/tracker/savefile.js
@@ -0,0 +1,66 @@
+/*
+ * SAVEFILE STRUCTURE
+ * ==================
+ *
+ * {
+ *   meta: {
+ *     users: {
+ *       <discord user id>: {
+ *         name: <user name>
+ *       }, ...
+ *     },
+ *
+ *     // the user index is an array of discord user ids,
+ *     // these indexes are used in the message objects to save space
+ *     userindex: [
+ *       <discord user id>, ...
+ *     ],
+ *
+ *     server: [
+ *       {
+ *         name: <server name>,
+ *         type: <"SERVER"|"DM">
+ *       }, ...
+ *     ],
+ *
+ *     channels: {
+ *       <discord channel id>: {
+ *         server: <server index in the metadata.server array>,
+ *         name: <channel name>
+ *       }, ...
+ *     }
+ *   },
+ *
+ *   data: {
+ *     <discord channel id>: {
+ *       <discord message id>: {
+ *         u: <user index of the sender>,
+ *         t: <message timestamp>,
+ *         m: <message content>,
+ *         f: <message flags>, // bit 1 = edited, bit 2 = has user mentions (omit for no flags),
+ *         e: [ // omit for no embeds
+ *           {
+ *             url: <embed url>,
+ *             type: <embed type>
+ *           }, ...
+ *         ],
+ *         a: [ // omit for no attachments
+ *           {
+ *             url: <attachment url>
+ *           }, ...
+ *         ]
+ *       }, ...
+ *     }, ...
+ *   }
+ * }
+ */
+
+var SAVEFILE = function(){
+  this.db = {};
+  this.db.meta = {};
+  this.db.data = {};
+};
+
+SAVEFILE.prototype.toJson = function(){
+  return JSON.stringify(this.db);
+};
