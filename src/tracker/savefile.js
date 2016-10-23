a8abb720a1d887a42469bb6d29ae3b6304dc451f
chylex <info@chylex.com>
2016-10-23 21:10:25 +0200

    Add user, server, and channel registration functions to savefile.js

--- src/tracker/savefile.js
+++ src/tracker/savefile.js
@@ -77,6 +77,52 @@ var SAVEFILE = function(){
   this.tmp = {};
   this.tmp.userlookup = {};
 };
+
+SAVEFILE.prototype.findOrRegisterUser = function(userId, userName){
+  if (!(userId in this.meta.users)){
+    this.meta.users[userId] = {
+      name: userName
+    };
+    
+    this.meta.userindex.push(userId);
+    return this.tmp.userlookup[userId] = this.meta.userindex.length-1;
+  }
+  else{
+    return this.tmp.userlookup[userId];
+  }
+};
+
+SAVEFILE.prototype.findOrRegisterServer = function(serverName, serverType){
+  var index = this.meta.servers.findIndex(server => server.name === serverName && server.type === serverType);
+  
+  if (index === -1){
+    this.meta.servers.push({
+      name: serverName,
+      type: serverType
+    });
+    
+    return this.meta.servers.length-1;
+  }
+  else{
+    return index;
+  }
+};
+
+SAVEFILE.prototype.tryRegisterChannel = function(serverIndex, channelId, channelName){
+  if (!this.meta.servers[serverIndex]){
+    return undefined;
+  }
+  else if (channelId in this.meta.channels){
+    return false;
+  }
+  else{
+    this.meta.channels[channelId] = {
+      server: serverIndex,
+      name: channelName
+    };
+    
+    return true;
+  }
 };
 
 SAVEFILE.prototype.toJson = function(){
