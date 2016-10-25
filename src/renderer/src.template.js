7f654eaf5df3134372b2caf20fc77b3626951261
chylex <info@chylex.com>
2016-10-25 22:46:55 +0200

    Add template.js to renderer for basic templating

---
+++ src/renderer/src.template.js
@@ -0,0 +1,18 @@
+var TEMPLATE_REGEX = /{([^{}]+?)}/g;
+
+var TEMPLATE = function(contents){
+  this.contents = contents;
+};
+
+TEMPLATE.prototype.apply = function(obj, processor){
+  return this.contents.replace(TEMPLATE_REGEX, (full, match) => {
+    var value = match.split(".").reduce((o, property) => o[property], obj);
+    
+    if (processor){
+      var updated = processor(match, value);
+      return typeof updated === "undefined" ? value : updated;
+    }
+    
+    return value;
+  });
+};
