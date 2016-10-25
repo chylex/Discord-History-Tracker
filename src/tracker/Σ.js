21f02b826a85f1f61afe4b90f1fe02885a2f5cb6
chylex <info@chylex.com>
2016-10-25 15:31:56 +0200

    Implement execution code with basic message tracking and GUI display

--- src/tracker/Σ.js
+++ src/tracker/Σ.js
@@ -1,1 +1,13 @@
-/* execution */
+DISCORD.setupMessageRequestHook((channel, messages) => {
+  if (STATE.isTracking()){
+    var info = DISCORD.getSelectedChannel();
+    
+    if (info.id == channel){ // Discord has a bug where the message request may be sent without switching channels
+      STATE.addDiscordChannel(info.server, info.type, channel, info.channel);
+      STATE.addDiscordMessages(channel, messages);
+    }
+  }
+});
+
+GUI.showController();
+GUI.showSettings();
