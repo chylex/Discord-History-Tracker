8ffdd8eaec01a2625a12d2159acb01b22d2fb425
chylex <info@chylex.com>
2016-11-01 16:37:26 +0100

    Fix style of code elements, detection of inline code, and prevent formatting inside them

--- src/renderer/scr.discord.js
+++ src/renderer/scr.discord.js
@@ -4,7 +4,7 @@ var DISCORD = (function(){
     formatItalic: /(.)?\*([\s\S]+?)\*(?!\*)/g,
     formatUnderline: /__([\s\S]+?)__(?!_)/g,
     formatStrike: /~~([\s\S]+?)~~(?!~)/g,
-    formatCodeInline: /`+\s*([\s\S]*?[^`])\s*\1(?!`)/g,
+    formatCodeInline: /(`+)\s*([\s\S]*?[^`])\s*\1(?!`)/g,
     formatCodeBlock: /```(?:([A-z0-9\-]+?)\n+)?\n*([^]+?)\n*```/g,
     formatUrl: /<?(\b(?:https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])>?/ig,
     specialEscaped: /\\([*_~`\\])/g,
@@ -76,12 +76,12 @@ var DISCORD = (function(){
           }
           
           var processed = DOM.escapeHTML(value)
+            .replace(REGEX.formatCodeBlock, (full, ignore, match) => "<code class='block'>"+match.replace(REGEX.specialUnescaped, "\\$1")+"</code>")
+            .replace(REGEX.formatCodeInline, (full, ignore, match) => "<code class='inline'>"+match.replace(REGEX.specialUnescaped, "\\$1")+"</code>")
             .replace(REGEX.formatBold, "<b>$1</b>")
             .replace(REGEX.formatItalic, (full, pre, match) => pre === '\\' ? full : pre+"<i>"+match+"</i>")
             .replace(REGEX.formatUnderline, "<u>$1</u>")
             .replace(REGEX.formatStrike, "<s>$1</s>")
-            .replace(REGEX.formatCodeBlock, "<code style='display:block'>$2</code>")
-            .replace(REGEX.formatCodeInline, "<code>$1</code>")
             .replace(REGEX.formatUrl, "<a href='$1' target='_blank' rel='noreferrer'>$1</a>")
             .replace(REGEX.mentionChannel, (full, match) => "<span class='link mention-chat'>#"+STATE.getChannelName(match)+"</span>")
             .replace(REGEX.mentionUser, (full, match) => "<span class='link mention-user'>@"+STATE.getUserName(match)+"</span>")
