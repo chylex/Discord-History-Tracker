5f6843a4ee6beb5ebb7b3d518bbdbb82b7884019
chylex <contact@chylex.com>
2017-09-20 12:45:00 +0200

    Fix instructions on the website to be clearer

--- web/index.php
+++ web/index.php
@@ -20,17 +20,18 @@
       <img src="img/tracker.png" width="851" class="dht">
       
       <h2>How to Save History</h2>
-      <h3>...in your browser</h3>
-      <p>Add the script as a browser bookmark by clicking <a rel="sidebar" title="Discord History Tracker" href="<?php include "./build/track.html"; ?>">Add Bookmark</a>. Then open <a href="https://discordapp.com/channels/@me">Discord</a> and click the bookmark.</p>
-      <p>In Firefox, make sure to uncheck &laquo;Load this bookmark in the sidebar&raquo; after clicking <strong>Add Bookmark</strong>.</p>
+      <h3>...using your browser</h3>
+      <p>In Firefox, click <a rel="sidebar" title="Discord History Tracker" href="<?php include "./build/track.html"; ?>">Add Bookmark</a> and uncheck &laquo;Load this bookmark in the sidebar&raquo;.</p>
+      <p>In Chrome, Edge, Safari, and most other browsers, you will need to manually copy the link address of the <a rel="sidebar" title="Discord History Tracker" href="<?php include "./build/track.html"; ?>">tracker script</a>, and use your browser's bookmark manager to add it as a bookmark.</p>
+      <p>After adding the script to your bookmarks, open <a href="https://discordapp.com/channels/@me">Discord</a> and click the bookmark to run the script. If you run into any issues, please make sure your browser is up-to-date.</p>
       
-      <h3>...in Discord app</h3>
+      <h3>...using the Discord app</h3>
       <p>Click <a href="javascript:" id="tracker-copy">Copy to Clipboard</a><textarea id="tracker-copy-contents"><?php include "./build/track.html"; ?></textarea> to copy the script. Then press <strong>Ctrl+Shift+I</strong> in the Discord app, select <strong>Console</strong>, paste the script into the text box at the bottom and press <strong>Enter</strong> to run it. You can then close the console and continue using the script.</p>
       
       <h3>What next?</h3>
-      <p>When running for the first time, you will see a <strong>Settings</strong> dialog with additional instructions that explain the limitations and recommendations. Please, read them carefully.</p>
-      <p>Upload your previously saved file if you have any. By default, Discord History Tracker is set to pause tracking after it reaches a previously saved message to avoid unnecessary history loading. You may also set it to load all channels in the server or your friends list by checking <strong>Switch to Next Channel</strong>.</p>
-      <p>Once you have configured everything, click <strong>Start Tracking</strong>, let it run in the background, and download the file when done.</p>
+      <p>When running for the first time, you will see a <strong>Settings</strong> dialog where you can configure the script's behavior. These settings will be remembered as long as you don't delete cookies in your browser.</p>
+      <p>By default, Discord History Tracker is set to pause tracking after it reaches a previously saved message to avoid unnecessary history loading. You may also set it to load all channels in the server or your friends list by selecting <strong>Switch to Next Channel</strong>.</p>
+      <p>Once you have configured everything, upload your previously saved file (if you have any), click <strong>Start Tracking</strong>, and let it run in the background. Once done, or after manually pausing it, you can download the generated file.</p>
       
       <h2>How to View History</h2>
       <p>To browse the saved text channels, open the <a href="build/viewer.html">Viewer</a> and upload the file. It is recommended to download the viewer and place it next to your saved files, that way you can view your history offline.</p>
@@ -53,7 +54,6 @@
       <h2>Disclaimer</h2>
       <p>Discord History Tracker and the viewer are fully client-side and do not communicate with any servers. If you close your browser while the script is running, all unsaved progress will be lost.</p>
       <p>Please, do not use this script for large or public servers. The script was made as a convenient way of keeping a local copy of private and group chats, as Discord is currently lacking this functionality.</p>
-      <p>Make sure you are using an up-to-date browser. Internet Explorer and old versions of good browsers are not supported.</p>
     </div>
     
     <script type="text/javascript">
