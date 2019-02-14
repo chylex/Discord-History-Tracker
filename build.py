4858a7915830804c8cc14afaf7ead245d9c39ddf
chylex <contact@chylex.com>
2019-02-14 18:05:38 +0100

    Release BETA v.11

--- build.py
+++ build.py
@@ -8,8 +8,8 @@ import os
 import distutils.dir_util
 
 
-VERSION_SHORT = "BETA v.10"
-VERSION_FULL = VERSION_SHORT + ", released 6 Feb 2019"
+VERSION_SHORT = "BETA v.11"
+VERSION_FULL = VERSION_SHORT + ", released 14 Feb 2019"
 
 
 EXEC_UGLIFYJS_WIN = "{2}/lib/uglifyjs.cmd --parse bare_returns --compress --mangle toplevel --mangle-props keep_quoted,reserved=[{3}] --output \"{1}\" \"{0}\""
