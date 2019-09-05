f2361a97699455d4c6b4b36b9c0e80e54c5cc36d
chylex <contact@chylex.com>
2019-09-05 22:20:54 +0200

    Fix UI being overlaid instead of pushing the bottom of the page up
    Closes #74

--- src/tracker/gui.js
+++ src/tracker/gui.js
@@ -83,7 +83,7 @@ var GUI = (function(){
       // styles
       
       controller.styles = DOM.createStyle(`
-.app, .connecting { bottom: 48px !important; }
+#app-mount > div[class*="app-"] { margin-bottom: 48px !important; }
 #dht-ctrl { position: absolute; bottom: 0; width: 100%; height: 48px; background-color: #FFF; }
 #dht-ctrl button { height: 32px; margin: 8px 0 8px 8px; font-size: 16px; padding: 0 12px; background-color: #7289DA; color: #FFF; text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.75); }
 #dht-ctrl button:disabled { background-color: #7A7A7A; cursor: default; }
