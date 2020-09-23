17cf87991c5ff197ffb1ca41350d7f310bb08797
chylex <contact@chylex.com>
2020-09-23 19:45:55 +0200

    Remove Google Analytics from official website

--- web/index.php
+++ web/index.php
@@ -95,10 +95,6 @@
     </div>
     
     <script type="text/javascript">
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-      ga('create','UA-48632978-5','auto');ga('send','pageview');
-      
       var contents = document.getElementById("tracker-copy-contents");
       var issue = document.getElementById("tracker-copy-issue");
       var button = document.getElementById("tracker-copy-button");
