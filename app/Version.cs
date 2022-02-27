c4fe6c4391ebb924b4229557c4d1fbc1bdbc2bbe
chylex <contact@chylex.com>
2022-02-27 16:20:30 +0100

    Move app version info out of .csproj and into a single linked .cs file

---
+++ app/Version.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using DHT.Utils;
+
+[assembly: AssemblyVersion(Version.Tag)]
+[assembly: AssemblyFileVersion(Version.Tag)]
+[assembly: AssemblyInformationalVersion(Version.Tag)]
+
+namespace DHT.Utils {
+	static class Version {
+		public const string Tag = "32.1.0.0";
+	}
+}
