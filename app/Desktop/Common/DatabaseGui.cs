217c1f9e1097f47270edd33c8c1c7b1e5b776d72
chylex <contact@chylex.com>
2022-03-05 18:43:48 +0100

    Tell users to backup database file(s) before a schema upgrade

--- app/Desktop/Common/DatabaseGui.cs
+++ app/Desktop/Common/DatabaseGui.cs
@@ -56,11 +56,11 @@ namespace DHT.Desktop.Common {
 		}
 
 		public static async Task<DialogResult.YesNo> ShowCanUpgradeDatabaseDialog(Window window) {
-			return await Dialog.ShowYesNo(window, "Database Upgrade", "This database was created with an older version of DHT. If you proceed, the database will be upgraded and will no longer open in previous versions of DHT. Do you want to proceed with the upgrade?");
+			return await Dialog.ShowYesNo(window, "Database Upgrade", "This database was created with an older version of DHT. If you proceed, the database will be upgraded and will no longer open in previous versions of DHT.\n\nPlease ensure you have a backup of the database. Do you want to proceed with the upgrade?");
 		}
 
 		public static async Task<DialogResult.YesNo> ShowCanUpgradeMultipleDatabaseDialog(Window window) {
-			return await Dialog.ShowYesNo(window, "Database Upgrade", "One or more databases were created with an older version of DHT. If you proceed, these databases will be upgraded and will no longer open in previous versions of DHT. Otherwise, these databases will be skipped. Do you want to proceed with the upgrade?");
+			return await Dialog.ShowYesNo(window, "Database Upgrade", "One or more databases were created with an older version of DHT. If you proceed, these databases will be upgraded and will no longer open in previous versions of DHT. Otherwise, these databases will be skipped.\n\nPlease ensure you have a backup of the databases. Do you want to proceed with the upgrade?");
 		}
 	}
 }
