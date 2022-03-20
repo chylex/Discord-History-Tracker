f3723ee43b0cd413b07fd01cfeb1cf4c292e74eb
chylex <contact@chylex.com>
2022-03-20 15:48:13 +0100

    Split browser-only version into a separate branch & update README

--- README.md
+++ README.md
@@ -1,48 +1,32 @@
 # Welcome
 
-All you need to **use Discord History Tracker** is either an up-to-date browser, or the [Discord desktop client](https://discord.com/download). Visit the [official website](https://dht.chylex.com) for instructions.
+For instructions on how to **use Discord History Tracker**, visit the [official website](https://dht.chylex.com).
 
 To **report an issue or suggestion**, first please see the [issues](https://github.com/chylex/Discord-History-Tracker/issues) page and make sure someone else hasn't already created a similar issue report. If you do find an existing issue, comment on it or add a reaction. Otherwise, either click [New Issue](https://github.com/chylex/Discord-History-Tracker/issues/new), or contact me via email [contact@chylex.com](mailto:contact@chylex.com) or Twitter [@chylexmc](https://twitter.com/chylexmc).
 
-If you are interested in **creating your own version** from the source code, continue reading the [build instructions](#Build-Instructions) below.
+If you are interested in **building from source code**, continue reading the [build instructions](#Build-Instructions) below.
+
+This branch is dedicated to the Discord History Tracker desktop app. If you are looking for the older browser-only version, visit the [master-browser-only](https://github.com/chylex/Discord-History-Tracker/tree/master-browser-only) branch.
 
 # Build Instructions
-
-Follow the steps below to create your own version of Discord History Tracker.
 
 ### Setup
 
 Fork the repository and clone it to your computer (if you've never used git, you can download the [GitHub Desktop](https://desktop.github.com) client to get started quickly).
 
-Now you can modify the source code:
-* `src/tracker/` contains JS files that are automatically combined into the **tracker bookmark/script**
-* `src/viewer/` contains HTML, CSS, JS files that are then combined into the **offline viewer page**
+Folder organization:
+* `app/` contains a Visual Studio solution for the desktop app
 * `lib/` contains utilities required to build the project
 * `web/` contains source code of the [official website](https://dht.chylex.com), which can be used as a template when making your own website
 
+To start editing source code for the desktop app, open `app/DiscordHistoryTracker.sln` in [Visual Studio](https://visualstudio.microsoft.com/downloads/) or [Rider](https://www.jetbrains.com/rider/).
+
 ### Building
 
-After you've done changes to the source code, you will need to build it. Before that, download and install:
-* (**required**) [Python 3](https://www.python.org/downloads)
-  * Use to run the build script
-* (optional) [Node + npm](https://nodejs.org/en) & command line [uglify-js](https://www.npmjs.com/package/uglify-js)
-  * Not required on Windows
-  * Only required for optional [JS minification](#Minification) on Linux/Mac
+To build a `Debug` version of the desktop app, there are no additional requirements.
 
-Now open the folder that contains `build.py` in a command line, and run `python build.py` to create a build with default settings. The following files will be created:
-* `bld/track.js` is the raw tracker script that can be pasted into a browser console
-* `bld/track.html` is the tracker script but sanitized for inclusion in HTML (see `web/index.php` for examples)
-* `bld/viewer.html` is the complete offline viewer
+To build a `Release` version of the desktop app, you will need [Python 3](https://www.python.org/downloads), which is used by the build process to launch `app/Resources/minify.py` script.
 
-You can tweak the build process using the following flags:
-* `python build.py --nominify` to disable [minification](#Minification)
+When creating `Release` builds on systems other than 64-bit Windows, you will also need [Node + npm](https://nodejs.org/en), and [uglify-js](https://www.npmjs.com/package/uglify-js) installed globally (`npm install uglify-js -g`). On 64-bit Windows, both Node and uglify-js are already included in the `lib/` folder for convenience.
 
-### Minification
-
-The build process automatically minifies JS using `UglifyJS@3`, and CSS using a custom minifier.
-
-* If the `--nominify` flag is used, minification will be completely disabled
-* If `uglify-js` is not available from the command line, JS minification will be skipped
-  * When building on Windows 64-bit, the build script will use the included Node runner and packages
-  * When building on Windows 32-bit, you will need to download [Node 32-bit](https://nodejs.org/en/download) and replace the included one in `lib/`
-  * When building on Linux/Mac, the build script will attempt to find `uglifyjs` in the command line
+To create `Release` builds ready for distribution, run the `app/build.bat` script on Windows, or `app/build.sh` script on other operating systems. This will create self-contained executables for each major operating system, and a portable version that works on all other systems but requires .NET 5 to be installed. All builds are placed in the `app/bin` folder.
--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="robots" content="index,follow">
     <meta name="author" content="chylex">
-    <meta name="description" content="Discord History Tracker - Browser script to save history of Discord servers and private conversations">
+    <meta name="description" content="Discord History Tracker - Save history of Discord servers and private conversations">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
     <title>Discord History Tracker</title>
@@ -13,14 +13,14 @@
   </head>
   <body>
     <div class="inner">
-      <h1>Discord History Tracker <span class="version">{{{version:web}}}</span>&nbsp;<span class="bar">|</span>&nbsp;<span class="notes"><a href="https://github.com/chylex/Discord-History-Tracker/wiki/Release-Notes">Release&nbsp;Notes</a></span></h1>
+      <h1>Discord History Tracker&nbsp;<span class="bar">|</span>&nbsp;<span class="notes"><a href="https://github.com/chylex/Discord-History-Tracker/releases">Release&nbsp;Notes</a></span></h1>
       <p>Discord History Tracker lets you save chat history in your servers, groups, and private conversations, and view it offline.</p>
-      <p>You can use Discord History Tracker either <a href="#browser-only">entirely in your browser</a>, or download a <a href="#desktop-app">desktop app</a> for Windows / Linux / Mac. While the browser-only method is simpler and works on any device that has a modern web browser, it has significant limitations and fewer features than the app. Please read about both methods below.</p>
-      
       <img src="img/tracker.png" width="851" class="dht bordered">
-  
-      <h2 id="desktop-app">Method 1: Desktop App</h2>
-      <p>The app can be downloaded from <a href="https://github.com/chylex/Discord-History-Tracker/releases">GitHub</a>. Every release includes 4 versions:</p>
+      <p>This page explains how to use the desktop app, which is available for Windows / Linux / Mac.</p>
+      <p>If you are looking for the older version of Discord History Tracker which only needs a browser or the Discord app, visit the page for the <a href="https://dht.chylex.com/browser-only">browser-only version</a>, however keep in mind that this version has <strong>significant limitations and fewer features</strong>.</p>
+      
+      <h2>How to Use</h2>
+      <p>The desktop app can be downloaded from <a href="https://github.com/chylex/Discord-History-Tracker/releases">GitHub</a>. Every release includes 4 versions:</p>
       <ul>
         <li><strong>win-x64</strong> is for Windows (64-bit)</li>
         <li><strong>linux-x64</strong> is for Linux (64-bit)</li>
@@ -28,89 +28,24 @@
         <li><strong>portable</strong> requires <a href="https://dotnet.microsoft.com/download/dotnet/5.0/runtime" rel="nofollow noopener">.NET 5</a> to be installed, but should run on any operating system supported by .NET</li>
       </ul>
       <p>For the non-portable versions: extract the <strong>DiscordHistoryTracker</strong> file into a folder, and double-click it to launch the app.<br>For the portable version: extract it into a folder, open the folder in a terminal and type: <code>dotnet DiscordHistoryTracker.dll</code></p>
-  
+      
       <h3>How to Track Messages</h3>
       <p>The app saves messages into a database file stored on your computer. When you open the app, you are given the option to create a new database file, or open an existing one.</p>
       <p>In the <strong>Tracking</strong> tab, click <strong>Copy Tracking Script</strong> to generate a tracking script that works similarly to the browser-only version of Discord History Tracker, but instead of saving messages in the browser, the tracking script sends them to the app which saves them in the database file.</p>
       <img src="img/app-tracker.png" class="dht bordered" alt="Screenshot of the App (Tracker tab)">
-      <p>See <strong>Option 2: Browser / Discord Console</strong> above for more detailed instructions on how to paste the tracking script into the browser or Discord app console.</p>
       <p>When using the script for the first time, you will see a <strong>Settings</strong> dialog where you can configure the script. These settings will be remembered as long as you don't delete cookies in your browser.</p>
       <p>By default, Discord History Tracker is set to automatically scroll up to load the channel history, and pause tracking if it reaches a previously saved message to avoid unnecessary history loading.</p>
-  
+      
       <h3>How to View History</h3>
       <p>In the <strong>Viewer</strong> tab, you can open a viewer in your browser, or save it as a file you can open in your browser later. You also have the option to apply filters to only view a portion of the saved messages.</p>
       <img src="img/app-viewer.png" class="dht bordered" alt="Screenshot of the App (Viewer tab)">
-  
+      
       <h3>Technical Details</h3>
       <ol>
         <li>The app uses SQLite, which means that you can use SQL to manually query or manipulate the database file.</li>
         <li>The app communicates with the script using an integrated server. The server only listens for local connections (i.e. connections from programs running on your computer, not the internet). When you copy the tracking script, it will contain a randomly generated token that ensures only the tracking script is able to talk to the server.</li>
         <li>You can use the <code>-port &lt;p&gt;</code> and <code>-token &lt;t&gt;</code> command line arguments to configure the server manually &mdash; otherwise, they will be assigned automatically in a way that allows running multiple separate instances of the app.</li>
       </ol>
-      
-      <h2 id="browser-only">Method 2: Browser-Only</h2>
-      <p>A tracking script will load messages according to your settings, and save them in your browser.</p>
-      <p>Because everything happens in your browser, if the browser tab is closed, or your browser or computer crashes, you will lose all progress. Your browser may also be unable to process large amounts of messages. If this is a concern, use the app method.</p>
-      
-      <h3>Setup the Tracking Script</h3>
-      <h4>Option 1: Userscript</h4>
-      <div class="quote">
-        <p><strong>Preferred option.</strong> Requires a browser addon, but DHT will stay up-to-date and be easily accessible on the Discord website.</p>
-        
-        <ol>
-          <li>Install a userscript manager addon:
-            <ul>
-              <li><a href="https://violentmonkey.github.io/get-it/">Violentmonkey</a> (Chrome)</li>
-              <li><a href="https://tampermonkey.net/">Tampermonkey</a> (Firefox, Edge, Chrome, Opera)</li>
-              <li>Due to browser bugs / limitations, DHT will not work in <strong>Firefox</strong> with Greasemonkey / Violentmonkey, and in <strong>Safari</strong> at all</li>
-            </ul>
-          </li>
-          <li>Click <a href="build/track.user.js">Install Userscript</a> to prompt an installation into the userscript manager</li>
-          <li>Open <a href="https://discord.com/channels/@me" rel="noreferrer">Discord</a>, and view any server, group, or private conversation (it will not appear in Friends list)</li>
-          <li>Click <strong>DHT</strong> in the top right corner:<br><img src="img/button.png" class="bordered"></li>
-        </ol>
-      </div>
-      
-      <h4>Option 2: Browser / Discord Console</h4>
-      <div class="quote">
-        <p>The console is the only way to use DHT directly in the desktop app.</p>
-        
-        <ol>
-          <li>Click <a href="javascript:" id="tracker-copy-button" onauxclick="return false;">Copy to Clipboard</a> to copy the tracking script
-            <noscript> (requires JavaScript)</noscript>
-          </li>
-          <li>Press <strong>Ctrl</strong>+<strong>Shift</strong>+<strong>I</strong> in your browser or the Discord app, and select the <strong>Console</strong> tab</li>
-          <li>Paste the script into the console, and press <strong>Enter</strong> to run it</li>
-          <li>Press <strong>Ctrl</strong>+<strong>Shift</strong>+<strong>I</strong> again to close the console</li>
-        </ol>
-        
-        <p id="tracker-copy-issue">Your browser may not support copying to clipboard, please try copying the script manually:</p>
-        <textarea id="tracker-copy-contents"><?php include './build/track.html'; ?></textarea>
-      </div>
-      
-      <h4>Option 3: Bookmarklet</h4>
-      <div class="quote">
-        <p>Requires Firefox 69 or newer.</p>
-        
-        <ol>
-          <li>Right-click <a href="<?php include './build/track.html'; ?>" onclick="return false;" onauxclick="return false;">Discord History Tracker</a></li>
-          <li>Select &laquo;Bookmark This Link&raquo; and save the bookmark</li>
-          <li>Open <a href="https://discord.com/channels/@me" rel="noreferrer">Discord</a> and click the bookmark to run the script</li>
-        </ol>
-      </div>
-      
-      <h4>Old Versions</h4>
-      <p>Whenever DHT is updated to work with a new version of Discord, it may no longer work with the previous version of Discord.</p>
-      <p>If you haven't received that Discord update yet, see <a href="https://github.com/chylex/Discord-History-Tracker/wiki/Release-Notes">Release Notes</a> for information about recent updates, and <a href="https://github.com/chylex/Discord-History-Tracker/wiki/Old-Versions">Old Versions</a> if you need to use an older version of DHT.</p>
-      
-      <h3>How to Track Messages</h3>
-      <p>When using the script for the first time, you will see a <strong>Settings</strong> dialog where you can configure the script. These settings will be remembered as long as you don't delete cookies in your browser.</p>
-      <p>By default, Discord History Tracker is set to automatically scroll up to load the channel history, and pause tracking if it reaches a previously saved message to avoid unnecessary history loading.</p>
-      <p>Before you <strong>Start Tracking</strong>, you may use <strong>Upload &amp; Combine</strong> to load messages from a previously saved archive file into the browser.</p>
-      <p>When you click <strong>Download</strong>, the browser will generate an archive file from saved messages, and lets you save it to your computer.</p>
-      
-      <h3>How to View History</h3>
-      <p>First, save the <a href="build/viewer.html">Viewer</a> file to your computer. Then you can open the downloaded viewer in your browser, click <strong>Load File</strong>, and select the archive to view.</p>
       
       <h2>External Links</h2>
       <p class="links">
@@ -121,32 +56,5 @@
         <a href="https://ko-fi.com/chylex">Support&nbsp;via&nbsp;Ko-fi</a>
       </p>
     </div>
-    
-    <script type="text/javascript">
-      var contents = document.getElementById("tracker-copy-contents");
-      var issue = document.getElementById("tracker-copy-issue");
-      var button = document.getElementById("tracker-copy-button");
-      
-      if (document.queryCommandSupported("copy")) {
-        contents.style.display = "none";
-        issue.style.display = "none";
-      }
-      
-      button.addEventListener("click", function() {
-        contents.style.display = "block";
-        issue.style.display = "block";
-        
-        contents.select();
-        document.execCommand("copy");
-        
-        button.innerHTML = "Copied to Clipboard";
-        contents.style.display = "none";
-        issue.style.display = "none";
-      });
-      
-      contents.addEventListener("click", function() {
-        contents.select();
-      });
-    </script>
   </body>
 </html>
