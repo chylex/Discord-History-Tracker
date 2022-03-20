155dd226cb629d7e71d3b1d2326ea1a6cb9b7238
chylex <contact@chylex.com>
2022-03-20 14:53:02 +0100

    Split browser-only version into a separate branch

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
 # Welcome
 
-All you need to **use Discord History Tracker** is either an up-to-date browser, or the [Discord desktop client](https://discord.com/download). Visit the [official website](https://dht.chylex.com) for instructions.
+This branch is dedicated to the browser-only version of **Discord History Tracker**. All you need to use it is either an up-to-date browser, or the [Discord desktop client](https://discord.com/download). Visit the [official website](https://dht.chylex.com/browser-only) for instructions.
 
 To **report an issue or suggestion**, first please see the [issues](https://github.com/chylex/Discord-History-Tracker/issues) page and make sure someone else hasn't already created a similar issue report. If you do find an existing issue, comment on it or add a reaction. Otherwise, either click [New Issue](https://github.com/chylex/Discord-History-Tracker/issues/new), or contact me via email [contact@chylex.com](mailto:contact@chylex.com) or Twitter [@chylexmc](https://twitter.com/chylexmc).
 
-If you are interested in **creating your own version** from the source code, continue reading the [build instructions](#Build-Instructions) below.
+If you are interested in **building from source code**, continue reading the [build instructions](#Build-Instructions) below.
 
 # Build Instructions
-
-Follow the steps below to create your own version of Discord History Tracker.
 
 ### Setup
 
 Fork the repository and clone it to your computer (if you've never used git, you can download the [GitHub Desktop](https://desktop.github.com) client to get started quickly).
 
+By default, cloning will default to the `master` branch which is dedicated to the desktop app. Make sure to switch to the `master-browser-only` branch.
+
 Now you can modify the source code:
 * `src/tracker/` contains JS files that are automatically combined into the **tracker bookmark/script**
 * `src/viewer/` contains HTML, CSS, JS files that are then combined into the **offline viewer page**
 * `lib/` contains utilities required to build the project
-* `web/` contains source code of the [official website](https://dht.chylex.com), which can be used as a template when making your own website
+* `web/` contains source code of the [official website](https://dht.chylex.com/browser-only), which can be used as a template when making your own website
 
 ### Building
 
--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="robots" content="index,follow">
@@ -14,14 +14,15 @@
   <body>
     <div class="inner">
       <h1>Discord History Tracker <span class="version">{{{version:web}}}</span>&nbsp;<span class="bar">|</span>&nbsp;<span class="notes"><a href="https://github.com/chylex/Discord-History-Tracker/wiki/Release-Notes">Release&nbsp;Notes</a></span></h1>
-      <p>Discord History Tracker is a browser script that lets you locally save chat history in your servers, groups, and private conversations.</p>
-      <p>When the script is active, it will load history of the selected text channel up to the first message, and let you download it for offline viewing in your browser.</p>
-      
+      <p>Discord History Tracker lets you save chat history in your servers, groups, and private conversations, and view it offline.</p>
       <img src="img/tracker.png" width="851" class="dht bordered">
+      <p>This page explains how to use Discord History Tracker entirely in your browser. While this method gets you started quicker and works on any device that has a modern web browser, it has <strong>significant limitations and fewer features</strong> than the <a href="https://dht.chylex.com">desktop app</a>.</p>
+      <p>Because everything happens in your browser, if the browser tab is closed, or your browser or computer crashes, you will lose all progress. Your browser may also crash or freeze if you have too many messages. If this is a concern, <a href="https://dht.chylex.com">use the desktop app</a> instead.</p>
       
-      <h2>How to Save History</h2>
-      <h3>Running the Script</h3>
+      <h2>How to Use</h2>
+      <p>A tracking script will load messages according to your settings, and temporarily save them in your browser. Once you finish tracking, the browser will create an archive file you can save to your disk, and open in an offline viewer later.</p>
       
+      <h3>Setup the Tracking Script</h3>
       <h4>Option 1: Userscript</h4>
       <div class="quote">
         <p><strong>Preferred option.</strong> Requires a browser addon, but DHT will stay up-to-date and be easily accessible on the Discord website.</p>
@@ -40,19 +41,21 @@
         </ol>
       </div>
       
-      <h4>Option 2: Browser Console</h4>
+      <h4>Option 2: Browser / Discord Console</h4>
       <div class="quote">
         <p>The console is the only way to use DHT directly in the desktop app.</p>
         
         <ol>
-          <li>Click <a href="javascript:" id="tracker-copy-button" onauxclick="return false;">Copy to Clipboard</a> to copy the script<noscript> (requires JavaScript)</noscript></li>
+          <li>Click <a href="javascript:" id="tracker-copy-button" onauxclick="return false;">Copy to Clipboard</a> to copy the tracking script
+            <noscript> (requires JavaScript)</noscript>
+          </li>
           <li>Press <strong>Ctrl</strong>+<strong>Shift</strong>+<strong>I</strong> in your browser or the Discord app, and select the <strong>Console</strong> tab</li>
           <li>Paste the script into the console, and press <strong>Enter</strong> to run it</li>
           <li>Press <strong>Ctrl</strong>+<strong>Shift</strong>+<strong>I</strong> again to close the console</li>
         </ol>
         
         <p id="tracker-copy-issue">Your browser may not support copying to clipboard, please try copying the script manually:</p>
-        <textarea id="tracker-copy-contents"><?php include "./build/track.html"; ?></textarea>
+        <textarea id="tracker-copy-contents"><?php include './build/track.html'; ?></textarea>
       </div>
       
       <h4>Option 3: Bookmarklet</h4>
@@ -60,38 +63,33 @@
         <p>Requires Firefox 69 or newer.</p>
         
         <ol>
-          <li>Right-click <a href="<?php include "./build/track.html"; ?>" onclick="return false;" onauxclick="return false;">Discord History Tracker</a></li>
+          <li>Right-click <a href="<?php include './build/track.html'; ?>" onclick="return false;" onauxclick="return false;">Discord History Tracker</a></li>
           <li>Select &laquo;Bookmark This Link&raquo; and save the bookmark</li>
           <li>Open <a href="https://discord.com/channels/@me" rel="noreferrer">Discord</a> and click the bookmark to run the script</li>
         </ol>
       </div>
       
       <h4>Old Versions</h4>
-      <div class="quote">
-        <p>Whenever DHT is fixed to work with a recent Discord update, it will no longer work on the previous version of Discord.</p>
-        <p>If you haven't received that Discord update yet, see <a href="https://github.com/chylex/Discord-History-Tracker/wiki/Release-Notes">Release Notes</a> for information about recent updates, and <a href="https://github.com/chylex/Discord-History-Tracker/wiki/Old-Versions">Old Versions</a> if you need to use an older version of DHT.</p>
-      </div>
+      <p>Whenever DHT is updated to work with a new version of Discord, it may no longer work with the previous version of Discord.</p>
+      <p>If you haven't received that Discord update yet, see <a href="https://github.com/chylex/Discord-History-Tracker/wiki/Release-Notes">Release Notes</a> for information about recent updates, and <a href="https://github.com/chylex/Discord-History-Tracker/wiki/Old-Versions">Old Versions</a> if you need to use an older version of DHT.</p>
       
-      <h3>Using the Script</h3>
-      <p>When running for the first time, you will see a <strong>Settings</strong> dialog where you can configure the script. These settings will be remembered as long as you don't delete cookies in your browser.</p>
-      <p>By default, Discord History Tracker is set to pause tracking after it reaches a previously saved message to avoid unnecessary history loading. You may also set it to load all channels in the server or your friends list by selecting <strong>Switch to Next Channel</strong>.</p>
-      <p>Once you have configured everything, upload your previously saved archive (if you have any), click <strong>Start Tracking</strong>, and let it run. After the script saves all messages, download the archive.</p>
+      <h3>How to Track Messages</h3>
+      <p>When using the script for the first time, you will see a <strong>Settings</strong> dialog where you can configure the script. These settings will be remembered as long as you don't delete cookies in your browser.</p>
+      <p>By default, Discord History Tracker is set to automatically scroll up to load the channel history, and pause tracking if it reaches a previously saved message to avoid unnecessary history loading.</p>
+      <p>Before you <strong>Start Tracking</strong>, you may use <strong>Upload &amp; Combine</strong> to load messages from a previously saved archive file into the browser.</p>
+      <p>When you click <strong>Download</strong>, the browser will generate an archive file from saved messages, and lets you save it to your computer.</p>
       
-      <h2>How to View History</h2>
-      <p>Download the <a href="build/viewer.html">Viewer</a>, open it in your browser, and load the archive. By downloading it to your computer, you can view archives offline, and allow the browser to load image previews that might otherwise not load if the remote server prevents embedding them.</p>
+      <h3>How to View History</h3>
+      <p>First, save the <a href="build/viewer.html">Viewer</a> file to your computer. Then you can open the downloaded viewer in your browser, click <strong>Load File</strong>, and select the archive to view.</p>
       
       <h2>External Links</h2>
       <p class="links">
         <a href="https://github.com/chylex/Discord-History-Tracker/issues">Issues&nbsp;&amp;&nbsp;Suggestions</a>&nbsp;&nbsp;&mdash;&nbsp;
-        <a href="https://github.com/chylex/Discord-History-Tracker">Source&nbsp;Code</a>&nbsp;&nbsp;&mdash;&nbsp;
+        <a href="https://github.com/chylex/Discord-History-Tracker/tree/master-browser-only">Source&nbsp;Code</a>&nbsp;&nbsp;&mdash;&nbsp;
         <a href="https://twitter.com/chylexmc">Follow&nbsp;Dev&nbsp;on&nbsp;Twitter</a>&nbsp;&nbsp;&mdash;&nbsp;
         <a href="https://www.patreon.com/chylex">Support&nbsp;via&nbsp;Patreon</a>&nbsp;&nbsp;&mdash;&nbsp;
         <a href="https://ko-fi.com/chylex">Support&nbsp;via&nbsp;Ko-fi</a>
       </p>
-      
-      <h2>Disclaimer</h2>
-      <p>Discord History Tracker and the viewer are fully client-side and do not communicate with any servers &ndash; the terms 'Upload' and 'Download' only refer to your browser. If you close your browser while the script is running, all unsaved progress will be lost.</p>
-      <p>Please, do not use this script for large or public servers. The script was made as a convenient way of keeping a local copy of private and group chats, as Discord is currently lacking this functionality.</p>
     </div>
     
     <script type="text/javascript">
@@ -99,12 +97,12 @@
       var issue = document.getElementById("tracker-copy-issue");
       var button = document.getElementById("tracker-copy-button");
       
-      if (document.queryCommandSupported("copy")){
+      if (document.queryCommandSupported("copy")) {
         contents.style.display = "none";
         issue.style.display = "none";
       }
       
-      button.addEventListener("click", function(){
+      button.addEventListener("click", function() {
         contents.style.display = "block";
         issue.style.display = "block";
         
@@ -116,9 +114,9 @@
         issue.style.display = "none";
       });
       
-      contents.addEventListener("click", function(){
+      contents.addEventListener("click", function() {
         contents.select();
-      })
+      });
     </script>
   </body>
 </html>
