fe588686fc83526dea333e8ca9b81c60bf93509d
chylex <contact@chylex.com>
2023-11-23 21:12:14 +0100

    Clean up project configuration and build scripts & fix CI builds

--- app/build.bat
+++ app/build.bat
@@ -4,11 +4,11 @@ set list=win-x64 linux-x64 osx-x64
 rmdir /S /Q bin
 
 (for %%a in (%list%) do (
-  dotnet publish Desktop -c Release -r %%a -o ./bin/%%a -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishReadyToRun=false -p:PublishTrimmed=true -p:TrimMode=partial -p:JsonSerializerIsReflectionEnabledByDefault=true --self-contained true
+  dotnet publish Desktop -c Release -r %%a -o ./bin/%%a -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishReadyToRun=false --self-contained true
   powershell "Compress-Archive -Path ./bin/%%a/* -DestinationPath ./bin/%%a.zip -CompressionLevel Optimal"
 ))
 
-dotnet publish Desktop -c Release -o ./bin/portable --self-contained false
+dotnet publish Desktop -c Release -o ./bin/portable -p:PublishTrimmed=false --self-contained false
 powershell "Compress-Archive -Path ./bin/portable/* -DestinationPath ./bin/portable.zip -CompressionLevel Optimal"
 
 echo Done
--- app/build.sh
+++ app/build.sh
@@ -17,9 +17,9 @@ rm -rf "./bin"
 configurations=(win-x64 linux-x64 osx-x64)
 
 for cfg in ${configurations[@]}; do
-	dotnet publish Desktop -c Release -r "$cfg" -o "./bin/$cfg" -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishReadyToRun=false -p:PublishTrimmed=true -p:TrimMode=partial -p:JsonSerializerIsReflectionEnabledByDefault=true --self-contained true
+	dotnet publish Desktop -c Release -r "$cfg" -o "./bin/$cfg" -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishReadyToRun=false --self-contained true
 	makezip "$cfg"
 done
 
-dotnet publish Desktop -c Release -o "./bin/portable" --self-contained false
+dotnet publish Desktop -c Release -o "./bin/portable" -p:PublishTrimmed=false --self-contained false
 makezip "portable"
