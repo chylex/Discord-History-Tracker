9bd105f57ef292cceb0dc47f71f4075e9181b147
chylex <contact@chylex.com>
2024-07-10 12:28:15 +0200

    Update About page with a link to issue tracker and libraries

--- app/Desktop/Main/AboutWindowModel.cs
+++ app/Desktop/Main/AboutWindowModel.cs
@@ -7,12 +7,24 @@ sealed class AboutWindowModel {
 		OpenUrl("https://dht.chylex.com");
 	}
 
+	public void ShowIssueTracker() {
+		OpenUrl("https://github.com/chylex/Discord-History-Tracker/issues");
+	}
+	
 	public void ShowSourceCode() {
 		OpenUrl("https://github.com/chylex/Discord-History-Tracker");
 	}
 
+	public void ShowLibraryNetCore() {
+		OpenUrl("https://github.com/dotnet/core");
+	}
+
 	public void ShowLibraryAvalonia() {
 		OpenUrl("https://www.nuget.org/packages/Avalonia");
+	}
+
+	public void ShowLibraryCommunityToolkit() {
+		OpenUrl("https://github.com/CommunityToolkit/dotnet");
 	}
 
 	public void ShowLibrarySqlite() {
@@ -23,8 +35,8 @@ sealed class AboutWindowModel {
 		OpenUrl("https://www.nuget.org/packages/Microsoft.Data.Sqlite");
 	}
 
-	public void ShowLibraryNetCore() {
-		OpenUrl("https://github.com/dotnet/core");
+	public void ShowLibraryRxNet() {
+		OpenUrl("https://github.com/dotnet/reactive");
 	}
 
 	private static void OpenUrl(string url) {
